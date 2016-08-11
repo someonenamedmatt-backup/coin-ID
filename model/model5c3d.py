4fadeee48b5da893e9183b41dfd1b7570d2fa817
Matt <schiffman.matt@gmail.com>
2016-08-11 14:52:05 -0700

    changed lr

--- model/model5c3d.py
+++ model/model5c3d.py
@@ -6,15 +6,15 @@ SEED = 22
 def encode_rad(input, n_labels, do=True):
     #do is dropouts, true for training, false for testing
     l = tf_helpers.get_radian_conv("conv1",input,3,3,32)
-    l = tf_helpers.get_radian_pool(l)
+    l = tf_helpers.get_radian_pool(l,1)
     l = tf_helpers.get_radian_conv("conv2",l,3,3,64)
-    l = tf_helpers.get_radian_pool(l)
+    l = tf_helpers.get_radian_pool(l,2)
     l = tf_helpers.get_radian_conv("conv3",l,3,3,128)
-    l = tf_helpers.get_radian_pool(l)
+    l = tf_helpers.get_radian_pool(l,3)
     l = tf_helpers.get_radian_conv("conv4",l,3,3,256)
-    l = tf_helpers.get_radian_pool(l)
+    l = tf_helpers.get_radian_pool(l,4)
     l = tf_helpers.get_radian_conv("conv5",l,3,3,512)
-    l = tf_helpers.get_radian_pool(l)
+    l = tf_helpers.get_radian_pool(l,5)
     l = tf_helpers.get_dense_layer_relu("dense1",l,256)
     if do:
        l = tf.nn.dropout(l,.5)
