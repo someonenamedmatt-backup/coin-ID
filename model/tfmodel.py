dcea08b6ac01d5c8497c0b072ef79550d231011b
Matt <schiffman.matt@gmail.com>
2016-08-11 14:39:07 -0700

    changed lr

--- model/tfmodel.py
+++ model/tfmodel.py
@@ -212,14 +212,14 @@ class TFModel(object):
                 for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
                     threads.extend(qr.create_threads(sess, coord=coord, daemon=True,
                                      start=True))
-                 num_iter = int(math.ceil(len(coinlabel.test_df) / self.batch_size))
-                 true_count = 0  # Counts the number of correct predictions.
-                 total_sample_count = num_iter * self.batch_size
-                 step = 0
-                 while step < num_iter and not coord.should_stop():
-                     predictions = sess.run([top_k_op])
-                     true_count += np.sum(predictions)
-                     step += 1
+                num_iter = int(math.ceil(len(coinlabel.test_df) / self.batch_size))
+                true_count = 0  # Counts the number of correct predictions.
+                total_sample_count = num_iter * self.batch_size
+                step = 0
+                while step < num_iter and not coord.should_stop():
+                    predictions = sess.run([top_k_op])
+                    true_count += np.sum(predictions)
+                    step += 1
                 # Compute precision @ 1.
                 precision = true_count / total_sample_count
                 print('%s: precision @ 1 = %.3f' % (datetime.now(), precision))
@@ -227,7 +227,7 @@ class TFModel(object):
                 summary.ParseFromString(sess.run(summary_op))
                 summary.value.add(tag='Precision @ 1', simple_value=precision)
                 summary_writer.add_summary(summary, global_step)
-            except Exception as e:  # pylint: disable=broad-except
-                coord.request_stop(e)
-            coord.request_stop()
-            coord.join(threads, stop_grace_period_secs=10)
+                except Exception as e:  # pylint: disable=broad-except
+                    coord.request_stop(e)
+                coord.request_stop()
+                coord.join(threads, stop_grace_period_secs=10)
