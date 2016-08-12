063340dde89a437e70d72a3f5a731e96160baacc
Matt <schiffman.matt@gmail.com>
2016-08-11 19:03:12 -0700

    added new network models

--- main_rad_cropped_big.py
+++ main_rad_cropped_big.py
@@ -3,7 +3,7 @@ from src.coinclasses.coinlabel import CoinLabel
 from model import modelalex as model
 
 def main():
-        tfm = TFModel(model.encode_rad, 'data/rad_cropped_5c3d', batch_size = 10)
+        tfm = TFModel(model.encode_rad, 'data/rad_cropped_network', batch_size = 10)
         coinlabel = CoinLabel('/data/images/', '/home/ubuntu/coin-ID/data/IDnamegrade.csv',
                                 'rad', 'grade_lbl', random_state = model.SEED)
         test = tfm.fit(coinlabel, 100)
--- model/model_network.py
+++ model/model_network.py
@@ -5,20 +5,20 @@ SEED = 22
 
 def encode_rad(input, n_labels, do=True):
     #do is dropouts, true for training, false for testing
-    l = tf_helpers.get_radian_conv("conv1",input,width = 5,height = 5, dim = 768, stride = 1)
-    l = tf_helpers.get_radian_conv("conv2",l,width = 1,height = 1, dim = 600, stride = 1)
-    l = tf_helpers.get_radian_conv("conv3",l,width = 1,height = 1, dim = 384, stride = 1)
+    l = tf_helpers.get_radian_conv("conv1",input,width = 5,height = 5, dim = 192, stride = 1)
+    l = tf_helpers.get_radian_conv("conv2",l,width = 1,height = 1, dim = 160, stride = 1)
+    l = tf_helpers.get_radian_conv("conv3",l,width = 1,height = 1, dim = 96, stride = 1)
     l = tf_helpers.get_radian_pool(l,1, ksize = 3, strides = 2)
     if do:
        l = tf.nn.dropout(l,.5)
-    l = tf_helpers.get_radian_conv("conv4",l,width = 5,height = 5, dim = 768, stride = 1)
-    l = tf_helpers.get_radian_conv("conv5",l,width = 1,height = 1, dim = 768, stride = 1)
-    l = tf_helpers.get_radian_conv("conv6",l,width = 1,height = 1, dim = 768, stride = 1)
+    l = tf_helpers.get_radian_conv("conv4",l,width = 5,height = 5, dim = 192, stride = 1)
+    l = tf_helpers.get_radian_conv("conv5",l,width = 1,height = 1, dim = 192, stride = 1)
+    l = tf_helpers.get_radian_conv("conv6",l,width = 1,height = 1, dim = 192, stride = 1)
     l = tf.nn.avg_pool(l, [1,3,3,1],[1,2,2,1],'VALID', name = 'avg_pool1')
     if do:
        l = tf.nn.dropout(l,.5)
-    l = tf_helpers.get_radian_conv("conv7",l,width = 3,height = 3, dim = 768, stride = 1)
-    l = tf_helpers.get_radian_conv("conv8",l,width = 1,height = 1, dim = 768, stride = 1)
+    l = tf_helpers.get_radian_conv("conv7",l,width = 3,height = 3, dim = 192, stride = 1)
+    l = tf_helpers.get_radian_conv("conv8",l,width = 1,height = 1, dim = 192, stride = 1)
     l = tf_helpers.get_radian_conv("conv9",l,width = 1,height = 1, dim = n_labels, stride = 1)
     l = tf.nn.avg_pool(l, [1,8,8,1],[1,1,1,1],'VALID', name = 'avg_pool2')
     l = tf.contrib.layers.flatten(l)
