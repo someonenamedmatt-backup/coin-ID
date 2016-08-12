b7e992f6143872e0088e73390095cc841bbca4f5
Matt <schiffman.matt@gmail.com>
2016-08-11 21:42:07 -0700

    stuff

--- main_rad_cropped.py
+++ main_rad_cropped.py
@@ -4,7 +4,7 @@ from model import model3c2d as model
 
 def main():
         tfm = TFModel(model.encode_rad, 'data/rad_cropped_3c2d_reg', batch_size = 25)
-        coinlabel = CoinLabel('/data2/processed/cropped/', '/home/ubuntu/coin/data/IDnamegrade.csv',
+        coinlabel = CoinLabel('/data/images/', '/home/ubuntu/coin-OD/data/IDnamegrade.csv',
                                 'rad', 'grade_lbl', random_state = model.SEED)
         test = tfm.fit(coinlabel, 100, load_save = False)
         # tfm.evaluate(coinlabel)
