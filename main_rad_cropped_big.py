3e7a52bdb91dbbb4a7af5c807037e17b1c50cdbe
Matt <schiffman.matt@gmail.com>
2016-08-11 18:55:37 -0700

    added new network models

--- main_rad_cropped_big.py
+++ main_rad_cropped_big.py
@@ -3,7 +3,7 @@ from src.coinclasses.coinlabel import CoinLabel
 from model import modelalex
 
 def main():
-        tfm = TFModel(model5c3d.encode_rad, 'data/rad_cropped_5c3d', batch_size = 10)
+        tfm = TFModel(modelalex.encode_rad, 'data/rad_cropped_5c3d', batch_size = 10)
         coinlabel = CoinLabel('/data/images/', '/home/ubuntu/coin-ID/data/IDnamegrade.csv',
                                 'rad', 'grade_lbl', random_state = model5c3d.SEED)
         test = tfm.fit(coinlabel, 100)
