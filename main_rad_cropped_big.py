9abdfd989d1da5bbbc85956c2295624d6d1d30b7
Matt <schiffman.matt@gmail.com>
2016-08-13 16:32:08 -0700

    balancing stuff

--- main_rad_cropped_big.py
+++ main_rad_cropped_big.py
@@ -3,10 +3,10 @@ from src.coinclasses.coinlabel import CoinLabel
 from model import model_network as model
 
 def main():
-        tfm = TFModel(model.encode_rad, 'data/saves/m_rad_cr_network_v0', batch_size = 10)
+        tfm = TFModel(model.encode_rad, 'data/saves/m_rad_cr_network_v1', batch_size = 25)
         coinlabel = CoinLabel('/data/images/', '/home/ubuntu/coin-ID/data/IDnamegrade.csv',
                                 'rad', 'grade_lbl', random_state = model.SEED)
-        test = tfm.fit(coinlabel, total_epochs = 25)
+        test = tfm.fit(coinlabel, total_epochs = 25,  balance_classes = True,)
 
         # tfm.evaluate(coinlabel)
 
