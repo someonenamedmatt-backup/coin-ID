5bf89886a3a227aada540bd8f424526440010061
Matt <schiffman.matt@gmail.com>
2016-08-13 14:24:42 -0700

    added random image distortions, balanced classes, and predict one

--- main_img_whole.py
+++ main_img_whole.py
@@ -3,12 +3,12 @@ from src.coinclasses.coinlabel import CoinLabel
 from model import model3c2d as model
 
 def main():
-        tfm = TFModel(model.encode_img, 'data/saves/m_img_wh_v0')
-        coinlabel = CoinLabel('/data/images/', '/home/ubuntu/coin-ID/data/IDnamegrade.csv',
-                                'img', 'grade_lbl', random_state = model.SEED)
-        test = tfm.fit(coinlabel, total_epochs = 25)
+    tfm = TFModel(model.encode_img, 'data/saves/m_img_wh_v0', batch_size = 100)
+    coinlabel = CoinLabel('/data/images/', '/home/ubuntu/coin-ID/data/IDnamegrade.csv',
+                            'img', 'grade_lbl', random_state = model.SEED)
+    tfm.fit(coinlabel, total_epochs = 50, load_save = True, balance_classes = True)
 
-        # tfm.evaluate(coinlabel)
+    # tfm.evaluate(coinlabel)
 
 if __name__ == '__main__':
     main()
