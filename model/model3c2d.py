b034fd407ab5e753b084db8fa0724693a7518399
Matt <schiffman.matt@gmail.com>
2016-08-13 18:52:39 -0700

    rewrote basic model

--- model/model3c2d.py
+++ model/model3c2d.py
@@ -3,7 +3,7 @@ import tf_helpers
 
 SEED = 22
 
-def encode_rad(input, n_labels, do=True, weight_decay = 0.04):
+def encode_rad(input, n_labels, do=True, weight_decay = 0.04, batch_size):
     #do is dropouts, true for training, false for testing
     l = tf_helpers.get_radian_conv("conv1",input,width = 3,height = 3, dim = 32)
     l = tf_helpers.get_radian_pool(l,1, ksize = 2)
@@ -31,61 +31,61 @@ def encoding_img(input, n_labels, do=True, batch_size):
         conv1 = tf.nn.relu(bias, name=scope.name)
         tfhelpers._activation_summary(conv1)
 
-  # pool1
-  pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
+    # pool1
+    pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
                          padding='SAME', name='pool1')
-  # norm1
-  norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
+    # norm1
+    norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
                     name='norm1')
 
-  # conv2
-  with tf.variable_scope('conv2') as scope:
-    kernel = _variable_with_weight_decay('weights',
+    # conv2
+    with tf.variable_scope('conv2') as scope:
+        kernel = _variable_with_weight_decay('weights',
                                          shape=[5, 5, 64, 64],
                                          stddev=5e-2,
                                          wd=0.0)
-    conv = tf.nn.conv2d(norm1, kernel, [1, 1, 1, 1], padding='SAME')
-    biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.1))
-    bias = tf.nn.bias_add(conv, biases)
-    conv2 = tf.nn.relu(bias, name=scope.name)
-    tfhelpers._activation_summary(conv2)
+        conv = tf.nn.conv2d(norm1, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = _variable_on_cpu('biases', [64], tf.constant_initializer(0.1))
+        bias = tf.nn.bias_add(conv, biases)
+        conv2 = tf.nn.relu(bias, name=scope.name)
+        tfhelpers._activation_summary(conv2)
 
-  # norm2
-  norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
+    # norm2
+    norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
                     name='norm2')
-  # pool2
-  pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1],
+    # pool2
+    pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1],
                          strides=[1, 2, 2, 1], padding='SAME', name='pool2')
 
-  # local3
-  with tf.variable_scope('local3') as scope:
-    # Move everything into depth so we can perform a single matrix multiply.
-    reshape = tf.reshape(pool2, [batch_size, -1])
-    dim = reshape.get_shape()[1].value
-    weights = _variable_with_weight_decay('weights', shape=[dim, 384],
-                                          stddev=0.04, wd=0.004)
-    biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
-    local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
-    tfhelpers._activation_summary(local3)
+    # local3
+    with tf.variable_scope('local3') as scope:
+        # Move everything into depth so we can perform a single matrix multiply.
+        reshape = tf.reshape(pool2, [batch_size, -1])
+        dim = reshape.get_shape()[1].value
+        weights = _variable_with_weight_decay('weights', shape=[dim, 384],
+                                              stddev=0.04, wd=0.004)
+        biases = _variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
+        local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
+        tfhelpers._activation_summary(local3)
 
-  # local4
-  with tf.variable_scope('local4') as scope:
-    weights = _variable_with_weight_decay('weights', shape=[384, 192],
-                                          stddev=0.04, wd=0.004)
-    biases = _variable_on_cpu('biases', [192], tf.constant_initializer(0.1))
-    local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
-    tfhelpers._activation_summary(local4)
+    # local4
+    with tf.variable_scope('local4') as scope:
+        weights = _variable_with_weight_decay('weights', shape=[384, 192],
+                                              stddev=0.04, wd=0.004)
+        biases = _variable_on_cpu('biases', [192], tf.constant_initializer(0.1))
+        local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
+        tfhelpers._activation_summary(local4)
 
-  # softmax, i.e. softmax(WX + b)
-  with tf.variable_scope('softmax_linear') as scope:
-    weights = _variable_with_weight_decay('weights', [192, n_labels],
-                                          stddev=1/192.0, wd=0.0)
-    biases = _variable_on_cpu('biases', [n_labels],
-                              tf.constant_initializer(0.0))
-    softmax_linear = tf.add(tf.matmul(local4, weights), biases, name=scope.name)
-    tfhelpers._activation_summary(softmax_linear)
+    # softmax, i.e. softmax(WX + b)
+    with tf.variable_scope('softmax_linear') as scope:
+        weights = _variable_with_weight_decay('weights', [192, n_labels],
+                                              stddev=1/192.0, wd=0.0)
+        biases = _variable_on_cpu('biases', [n_labels],
+                                  tf.constant_initializer(0.0))
+        softmax_linear = tf.add(tf.matmul(local4, weights), biases, name=scope.name)
+        tfhelpers._activation_summary(softmax_linear)
 
-  return softmax_linear
+    return softmax_linear
 
 def _variable_on_cpu(name, shape, initializer):
   """Helper to create a Variable stored on CPU memory.
--- model/tfinput.py
+++ model/tfinput.py
@@ -2,10 +2,14 @@ from __future__ import division
 import tensorflow as tf
 SIZE = (64,64)
 
-def read_coin(filename_queue, distort_img):
+def read_coin(filename_queue, distort_img, transform = None):
     """Reads and parses coin data
       Args:
     filename_queue: A queue of strings with the filenames to read from.
+    distort img (whether to distory the image randomly)
+    transform image: None (converts to 64x64)
+                    'crp' crops and centers the image
+                    'rad' crops and centers the image then transforms radially
     Returns:
         An object representing a single example, with the following fields:
         height: number of rows in the result (128)
@@ -14,6 +18,7 @@ def read_coin(filename_queue, distort_img):
         grade: an int32 Tensor with the condition of the coin label
         name: an int32 Tensor with the name of the coin label
         uint8image: a [height, width, depth] float64 Tensor with the image data
+
      """
     class ImageRecord(object):
         pass
@@ -30,11 +35,10 @@ def read_coin(filename_queue, distort_img):
     result.key, value = reader.read(filename_queue)
     decoded = tf.cast(tf.decode_raw(value, tf.float64),tf.float32)
 
-
-
     result.grade = tf.cast(tf.slice(decoded, [image_bytes],[label_bytes]), tf.int32)
     result.name = tf.cast(tf.slice(decoded, [image_bytes+1],[label_bytes]), tf.int32)
     result.image = tf.image.resize_images(tf.reshape(tf.slice(decoded, [0], [image_bytes]),[result.height,result.width,result.depth]),SIZE[0],SIZE[1])
+
     if distort_img:
           result.image = tf.image.random_flip_left_right(result.image)
             # Randomly flip the image horizontally.
@@ -81,5 +85,64 @@ def input(file_list, batch_size, distort_img = False):
                                 capacity=batch_size * 4)
 
     # Display the training images in the visualizer.
-    # tf.image_summary('images', image_batch)
+    tf.image_summary('images', image_batch)
     return image_batch, tf.reshape(grade_batch, [batch_size]), tf.reshape(name_batch, [batch_size])
+
+SEARCH_MIN = 1.0
+SEARCH_MAX = 500.0
+MIN_RADIUS = 50
+
+def _find_circle(img):
+    #Takes the image and tries to find circles in it
+    #plays around with sensitivity until exactly one circle is found
+    #Colors everything outside of that circle white
+    #returns false if we don't find a circle
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100, param2 = SEARCH_MIN, minRadius = MIN_RADIUS)
+    if circles is None:
+        return False
+    circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100, param2 = SEARCH_MAX, minRadius = MIN_RADIUS)
+    g_1 = SEARCH_MAX
+    g_0 = SEARCH_MIN
+    counter = 0
+    while circles is None or len(circles[0])>1:
+        if counter >= 20:
+            raise False
+        param2 = (g_1+g_0)/2
+        circles = cv2.HoughCircles(gray, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 100, param2 = param2, minRadius = MIN_RADIUS)
+        if circles is None:
+            # guess higher
+            g_1 =  param2
+        else:
+            #guess loawer
+            g_0 = param2
+        counter += 1
+    x, y, r = np.round(circles[0,0]).astype(int)
+    cv2.circle(img, center = (x, y), radius = r+120, color= (255, 255, 255), thickness = 240)
+    return img
+
+def _resize_image(img, size = (128,128)):
+    #takes an image (length x width x 3 with rgb colors)
+    #and a tuple of size in pixels
+    #returns the image normalized in hsv coloring
+    #which cuts out whitespace and centers the image
+    #returns false if something doesn't work
+    if img.shape[-1] != 3:
+        return False
+    hsv = colors.rgb_to_hsv(img)
+    w = np.nonzero((hsv[:,:,2] < 160) & (hsv[:,:,1] < 160))
+    min_x, min_y = np.min(w,axis=1)
+    max_x, max_y = np.max(w,axis=1)
+    ims = misc.imresize(img[min_x:max_x,min_y:max_y,:],size)/256.0
+    return ims
+
+def _convert_to_radian(img):
+    #transforms the image radially
+    max=(2.0, img.shape[1]/(2*math.pi), 1)
+    mid=(img.shape[0]/2.0,img.shape[1]/2.0,0)
+    def polar_to_euclidean(pos):
+        pos = (pos[0]/max[0],pos[1]/max[1],pos[2])
+        nPos = (pos[0]*math.cos(pos[1]) + mid[0], pos[0]*math.sin(pos[1]) + mid[1],pos[2])
+        return nPos
+    rImg = scipy.ndimage.interpolation.geometric_transform(img,polar_to_euclidean, img.shape)
+    return rImg
