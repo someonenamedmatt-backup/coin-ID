ed497fce737ac80df252195004c8181ec41b0eac
Matt <schiffman.matt@gmail.com>
2016-08-13 18:57:50 -0700

    rewrote basic model

--- main_img_whole.py
+++ main_img_whole.py
@@ -3,7 +3,7 @@ from src.coinclasses.coinlabel import CoinLabel
 from model import model3c2d as model
 
 def main():
-    tfm = TFModel(model.encode_img, 'data/saves/m_img_wh_v2', batch_size = 100)
+    tfm = TFModel(model.encode_img, 'data/saves/m_img_wh_v3', batch_size = 100)
     coinlabel = CoinLabel('/data/images/', '/home/ubuntu/coin-ID/data/IDnamegrade.csv',
                             'img', 'grade_lbl', random_state = model.SEED)
     tfm.fit(coinlabel, total_epochs = 25, load_save = False, balance_classes = True, do = False)
