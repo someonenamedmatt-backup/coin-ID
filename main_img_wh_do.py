c9dbcbcd0f488da2cd5d803ad0ef74de43c50c01
Matt <schiffman.matt@gmail.com>
2016-08-13 19:45:18 -0700

    rewrote basic model

--- main_img_wh_do.py
+++ main_img_wh_do.py
@@ -6,7 +6,7 @@ def main():
     tfm = TFModel(model.encode_img, 'data/saves/m_img_wh_do_v0', batch_size = 100)
     coinlabel = CoinLabel('/data/images/', '/home/ubuntu/coin-ID/data/IDnamegrade.csv',
                             'img', 'grade_lbl', random_state = model.SEED)
-    tfm.fit(coinlabel, total_epochs = 25,grade = False, load_save = False, balance_classes = True, do = True, weight_decay = 0.0)
+    tfm.fit(coinlabel, total_epochs = 25, load_save = False, balance_classes = True, do = True, weight_decay = 0.0)
 
     # tfm.evaluate(coinlabel)
 
