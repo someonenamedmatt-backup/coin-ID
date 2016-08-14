0b09f2ae6a3f203f5bf6a49bfafb361520963ac8
Matt <schiffman.matt@gmail.com>
2016-08-13 21:16:49 -0700

    rewrote basic model

--- main_rad_cropped_big.py
+++ main_rad_cropped_big.py
@@ -3,10 +3,10 @@ from src.coinclasses.coinlabel import CoinLabel
 from model import model_network as model
 
 def main():
-        tfm = TFModel(model.encode_img, 'data/saves/m_img_wh_network_v0', batch_size = 25)
+        tfm = TFModel(model.encode_img, 'data/saves/m_img_wh_network_v2', batch_size = 25)
         coinlabel = CoinLabel('/data/images/', '/home/ubuntu/coin-ID/data/IDnamegrade.csv',
                                 'rad', 'grade_lbl', random_state = model.SEED)
-        test = tfm.fit(coinlabel, total_epochs = 25,  balance_classes = True,)
+        test = tfm.fit(coinlabel, total_epochs = 25,  balance_classes = True)
 
         # tfm.evaluate(coinlabel)
 
