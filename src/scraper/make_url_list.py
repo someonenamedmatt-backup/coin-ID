00651aeaa90bb23c8e2b403637287eb636a11ca5
Matt <schiffman.matt@gmail.com>
2016-08-22 12:12:44 -0700

    rewrite to v1, updated makeurl

--- src/scraper/make_url_list.py
+++ src/scraper/make_url_list.py
@@ -1,32 +1,47 @@
-import urllib
-import urllib2
-import sys
-import os
+'''
+Call make_url_csv to run
+It works by using the ebay page for "us coins"
+and breaks it down by subcategories then finding the search results for all subcategories
+'''
+
 import pandas as pd
 import threading
 from bs4 import BeautifulSoup
 import numpy as np
 import requests
 import multiprocessing as mp
-ebay_jargon = '?_fsrp=1&_trkparms=pageci%3D0a207eed-6535-467e-a540-1a0f99e619a0%2Cparentrq%3D4d9966fc1560abd824e3a2c1ffffeeee%2Ciid%3D1%2Cobjectid%3D173593&_dcat=173593&Certification=PCGS&rt=nc&_pgn='
-coin_start_page = 'http://www.ebay.com/rpp/coins-us?_fsrp=2'
 
+coin_start_page = 'http://www.ebay.com/sch/US-Coins/253/bn_1848949/i.html'
+ebay_jargon = '?_fsrp=1&_trkparms=pageci%3D9b814cfc-b615-4dbe-a518-a90728a15a9e%2Cparentrq%3Db3a55ed81560a860e4136df0fffc7d78%2Ciid%3D1%2Cobjectid%3D173591&_skc=50&rt=nc&_pgn=2'
 d = mp.Manager().list()
-#### Not using below right now
-search_terms = ['MS%252070', 'MS%252069', 'MS%252068', 'MS%252067',
-'MS%252066', 'MS%252065', 'MS%252064', 'MS%252063',
-'MS%252062', 'MS%252061', 'MS%252060', 'PR%252070',
- 'PR%252069', 'PR%252068', 'PR%252067', 'PR%252066',
-  'PR%252065', 'PR%252064', 'PR%252063', 'PR%252062',
-   'PR%252061', 'AU%252058', 'AU%252055', 'AU%252053',
-    'AU%252050', 'XF%252045', 'XF%252040', 'VF%252035',
-     'VF%252030', 'VF%252025', 'VF%252020', 'F%252015',
-     'F%252012', 'VG%252010', 'VG%25208', 'G%25206', 'G%25204',
-      'AG%25203', 'FA%25202', 'P%25201']
+
+def make_url_csv(filename):
+    '''
+    Creates a csv file of URLs which list ebay listings
+    INPUT: filename (full path) to save the output
+    OUTPUT: saves a csv of all listings
+    '''
+    print "Making URL List"
+    search_terms =  get_starting_pages()
+    jobs = []
+    for term in search_terms:
+        thread = threading.Thread(name=term, target=search_one_page, args=(term, ))
+        jobs.append(thread)
+        thread.start()
+    for j in jobs:
+        j.join()
+    print "Completed"
+    df = pd.DataFrame(list(d), columns = ['url'])
+    df.to_csv(filename, encoder='utf-8')
+    return df
+
 
 def get_starting_pages():
-    #searches starting at coin_start_page for coin subcategories
-    #Makes a list of each cateogories page (limited to PCGA certed coins)
+    '''
+    Searches through all subcategories to get the base urls of types of coins
+    INPUT: NONE
+    OUTPUT: A list of strings each of which is the start of a url that is iterable with pages
+    '''
     searches = []
     soup = BeautifulSoup(requests.get(coin_start_page).text,'html.parser')
     coin_pages = soup.findAll('li', {'data-node-id':True})[1:-7]
@@ -42,6 +57,11 @@ def get_starting_pages():
     return searches
 
 def build_list_of_links(link):
+    '''
+    Finds all the ebay listing from a given page
+    INPUT: url of an ebay search page
+    OUTPUT: List containing the urls of all ebay listings from a given search page
+    '''
      page = requests.get(link).text
      soup = BeautifulSoup(page,'html.parser')
      list_of_links = []
@@ -51,6 +71,11 @@ def build_list_of_links(link):
 
 
 def make_url_generator(starting_link):
+    '''
+    Creates a generator of additional pages of ebay listings
+    INPUT: string containing the start of a category of ebay listings
+    OUTPUT: generator which iterates through pages of the search
+    '''
     i = 0
     link = lambda i: starting_link+ebay_jargon + str(i)
     while True:
@@ -58,6 +83,11 @@ def make_url_generator(starting_link):
         yield link(i)
 
 def search_one_page(url):
+    '''
+    Finds all the URLS for a given base search
+    INPUT: base url (string), multiprocessing list in namespace
+    OUTPUT: extends multiprocessed list of strings
+    '''
     link_generator = make_url_generator(url)
     link = link_generator.next()
     temp_link_list = build_list_of_links(link)
@@ -71,20 +101,7 @@ def search_one_page(url):
     print "Finished {}".format(url)
     d.extend(link_list)
 
-def make_url_csv():
-    print "Making URL List"
-    search_terms =  get_starting_pages()
-    jobs = []
-    for term in search_terms:
-        thread = threading.Thread(name=term, target=search_one_page, args=(term, ))
-        jobs.append(thread)
-        thread.start()
-    for j in jobs:
-        j.join()
-    print "Completed"
-    df = pd.DataFrame(list(d),columns = ['url'])
-    df.to_csv('/home/ubuntu/coin/urls.csv',encoder='utf-8')
-    return df
+
 
 
 
